ff44cd9099c98dd7a4017957eeb375c9a668a29a
DinizSa <39380608+DinizSa@users.noreply.github.com>
2023-09-14 16:07:39 +0100

    First commit

---
+++ PleaseTanks/main.cpp
@@ -0,0 +1,125 @@
+#include <SFML/Graphics.hpp>
+
+#include <iostream>
+
+int main()
+{
+    int windowWidth = 600;
+    int windowHeight = 600;
+    sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight), "SFML works!");
+//    sf::CircleShape shape(windowWidth/4);
+//    shape.setFillColor(sf::Color::Green);
+//    shape.setPosition(windowWidth/4, windowHeight/4);
+    
+    int tankWidth = 200;
+    int tankHeight = 200;
+    int posX = 300.f;
+    int posY = 200.f;
+    float speed = 2.5f;
+    float tracksAngularSpeed = 90.f;
+    float gunAngularSpeed = 45.f;
+    
+//    Tracks
+    sf::Texture trackTexture;
+    if (!trackTexture.loadFromFile("/Users/Shared/merda/PleaseTanks/images/Track_1_A.png")) {
+        std::cout << "Error \n";
+    }
+    sf::RectangleShape rectTrackA, rectTrackB;
+//    rectTrackA.setSize({0.15f * tankWidth, 0.9f * tankHeight});
+//    rectTrackA.setTexture(&trackTexture);
+//    rectTrackA.setOrigin(tankWidth/2, tankHeight/2);
+//    rectTrackA.setPosition(posX + 0.24f * tankWidth, posY + 0.05f * tankHeight);
+    
+    float trackW = 0.15f * tankWidth;
+    float trackH = 0.9f * tankHeight;
+    rectTrackB.setSize({trackW, trackH});
+    rectTrackB.setTexture(&trackTexture);
+    rectTrackB.setOrigin(trackW / 2, trackH / 2);
+    rectTrackB.setPosition(posX + tankWidth/4, posY);
+
+//    Hull
+    sf::RectangleShape rectHull;
+    rectHull.setSize({200, 200});
+    sf::Texture hullTexture;
+    if (!hullTexture.loadFromFile("/Users/Shared/merda/PleaseTanks/images/Hull_03.png")) {
+        std::cout << "Error \n";
+    }
+    rectHull.setTexture(&hullTexture);
+    rectHull.setOrigin(tankWidth/2, tankHeight/2);
+    rectHull.setPosition(posX, posY);
+    
+    //    Gun
+    sf::RectangleShape rectGun;
+    rectGun.setSize({200, 200});
+    sf::Texture gunTexture;
+    if (!gunTexture.loadFromFile("/Users/Shared/merda/PleaseTanks/images/Gun_01.png")) {
+        std::cout << "Error \n";
+    }
+    rectGun.setTexture(&gunTexture);
+    rectGun.setOrigin(tankWidth/2, tankHeight*0.6f);
+    rectGun.setPosition(posX, posY);
+    
+    
+    sf::Transform tankTransform;
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+            if (event.type == sf::Event::KeyPressed) {
+                std::cout << "key pressed: " << event.key.scancode << '\n';
+                switch (event.key.scancode) {
+                    case sf::Keyboard::Scan::Escape:
+                        window.close();
+                        break;
+                    case sf::Keyboard::Scan::E:
+                        rectGun.rotate(gunAngularSpeed);
+                        break;
+                    case sf::Keyboard::Scan::Q:
+                        rectGun.rotate(-gunAngularSpeed);
+                        break;
+                    case sf::Keyboard::Scan::A:
+                        rectGun.rotate(-tracksAngularSpeed);
+                        rectTrackA.rotate(-tracksAngularSpeed);
+                        rectTrackB.rotate(-tracksAngularSpeed);
+                        rectHull.rotate(-tracksAngularSpeed);
+                        break;
+                    case sf::Keyboard::Scan::D:
+                        tankTransform.rotate(tracksAngularSpeed, {rectGun.getPosition().x, rectGun.getPosition().y});
+//                        rectGun.rotate(tracksAngularSpeed);
+//                        rectTrackA.rotate(tracksAngularSpeed);
+//                        rectTrackB.rotate(tracksAngularSpeed);
+//                        rectHull.rotate(tracksAngularSpeed);
+                        break;
+                    case sf::Keyboard::Scan::S:
+                        rectGun.move(0, speed);
+                        rectTrackA.move(0, speed);
+                        rectTrackB.move(0, speed);
+                        rectHull.move(0, speed);
+                        break;
+                    case sf::Keyboard::Scan::W:
+                        rectGun.move(0, -speed);
+                        rectTrackA.move(0, -speed);
+                        rectTrackB.move(0, -speed);
+                        rectHull.move(0, -speed);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+        
+
+        window.clear();
+        window.draw(rectTrackA, tankTransform);
+        window.draw(rectTrackB, tankTransform);
+        window.draw(rectHull, tankTransform);
+        window.draw(rectGun, tankTransform);
+        window.display();
+    }
+
+    return 0;
+}
